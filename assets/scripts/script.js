10804b794cc7dd2c7d499e58133f5bfa05b8f685
Isabella-Mitchell <i.mitchell.0707@gmail.com>
2022-06-22 16:59:08 +0000

    Update spacing CSS for question box and turn results box

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -291,35 +291,35 @@ function startNewGame() {
 const questionBank = {
     blackBird: {
         birdName: "Blackbird",
-        outlineImageSrc: "assets/images/Bird-outline-example.png",
-        finishedImageSrc: "assets/images/Bird-image-sample-5.png",
+        outlineImageSrc: "assets/images/bb-outline.png",
+        finishedImageSrc: "assets/images/bb-final.png",
         funFact: "This is a male blackbirds. Female blackbirds...",
         quiz: [
             {
                 question: "What size is the bird?",
                 genre: "size",
-                imageSrc: "assets/images/Bird-image-sample-1.png",
+                imageSrc: "assets/images/bb-size.png",
                 options: ["Smaller than 15 cm", "Between 15cm and 30cm", "Between 30cm and 70cm", "Larger than 70cm"],
                 correctAnswer: "Between 15cm and 30cm"
             },
             {
                 question: "What colour is the bird?",
                 genre: "colour",
-                imageSrc: "assets/images/Bird-image-sample-2.png",
+                imageSrc: "assets/images/bb-colour.png",
                 options: ["Black", "White", "Green", "Blue"],
                 correctAnswer: "Black"
             },
             {
                 question: "Where can you see this bird?",
                 genre: "location",
-                imageSrc: "assets/images/Bird-image-sample-3.png",
+                imageSrc: "assets/images/bb-action.png",
                 options: ["By or in fresh water", "In the Mountains or Highlands", "In a park or garden", "By the coast"],
                 correctAnswer: "In a park or garden"
             },
             {
                 question: "What distinctive feature does this bird have?",
                 genre: "features",
-                imageSrc: "assets/images/Bird-image-sample-4.png",
+                imageSrc: "assets/images/bb-feature.png",
                 options: ["A red chest", "A yellow or orange beak", "A spotted back", "Large crest feathers"],
                 correctAnswer: "A yellow or orange beak"
             }
